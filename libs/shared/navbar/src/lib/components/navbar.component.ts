f9345cde6409e5afc62f106838591dfe45c965dc
michael-otieno <otieno.oduor.michael@gmail.com>
2024-09-26 00:12:32 +0300

    add navbar

--- libs/shared/navbar/src/lib/components/navbar.component.ts
+++ libs/shared/navbar/src/lib/components/navbar.component.ts
@@ -4,5 +4,8 @@ import { Component } from '@angular/core';
   selector: 'lib-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
+  host: {
+    '[attr.data-navbar-instance]': 'true', // This forces unique IDs for the instances
+  },
 })
 export class NavbarComponent {}
