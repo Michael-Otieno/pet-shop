0d30317157939093d797b87cc3ba90041d3b8e03
michael-otieno <otieno.oduor.michael@gmail.com>
2024-09-26 11:25:49 +0300

    add login and signup

--- apps/pages/auth/auth/src/app/remote-entry/entry.module.ts
+++ apps/pages/auth/auth/src/app/remote-entry/entry.module.ts
@@ -6,10 +6,17 @@ import { RemoteEntryComponent } from './entry.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { remoteRoutes } from './entry.routes';
 import { NavbarModule } from '@furn-shop/navbar';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [RemoteEntryComponent, NxWelcomeComponent],
-  imports: [CommonModule, RouterModule.forChild(remoteRoutes),NavbarModule],
+  declarations: [
+    RemoteEntryComponent,
+    NxWelcomeComponent,
+    SignupComponent,
+    LoginComponent,
+  ],
+  imports: [CommonModule, RouterModule.forChild(remoteRoutes), NavbarModule],
   providers: [],
 })
 export class RemoteEntryModule {}
