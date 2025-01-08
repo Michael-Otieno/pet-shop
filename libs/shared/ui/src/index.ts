d0c30a05979b63decc443fb0aa3484aeea5bc433
michael-otieno <otieno.oduor.michael@gmail.com>
2025-01-08 09:01:54 +0300

    add btn component

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/ui.module';
 export * from './lib/components/hero-section/hero-section.component';
 
 export * from './lib/components/item-card/item-card.component';
+
+export * from './lib/components/button/button.component';
